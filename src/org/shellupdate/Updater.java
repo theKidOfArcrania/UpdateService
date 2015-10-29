8160572d05b8595b881bdc0162b98a18c940ac66
theKidOfArcrania <wanghenry1998@gmail.com>
2015-10-28 23:39:54 -0500

    Add jar signing

--- src/org/shellupdate/Updater.java
+++ src/org/shellupdate/Updater.java
@@ -1,22 +1,34 @@
 package org.shellupdate;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.security.KeyStore;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Scanner;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
 
 public class Updater {
 	private static Properties params;
 
 	public static void main(String[] args) throws IOException {
+		byte[] read = new byte[100];
+		System.in.read(read);
+		System.out.println();
+		System.out.println(new String(read));
+	}
+
+	public static void stuff(String[] args) throws IOException {
 		params.load(ClassLoader.getSystemResourceAsStream("params.PROPERTIES"));
 
-		Scanner in = new Scanner(System.in);
+		Scanner scrn = new Scanner(System.in);
 		if (args.length != 1) {
 			System.out.println("Usage: java -jar Update.jar [New-jar version]");
 			System.exit(1);
@@ -34,7 +46,7 @@ public class Updater {
 		String updateName = "";
 		while (updateName.matches("[A-Za-z1-0]+") && updateFile.exists()) {
 			System.out.print("Enter update name: ");
-			updateName = in.nextLine();
+			updateName = scrn.nextLine();
 			if (updateName.isEmpty()) {
 				return;
 			}
@@ -44,16 +56,28 @@ public class Updater {
 		System.out.println();
 
 		try {
+			int lenRead;
+			byte[] buffer = new byte[8196];
+
 			JarFile oldVersion = new JarFile(oldShell);
 			JarFile newVersion = new JarFile(newShell);
 
-			Enumeration<JarEntry> entries = newVersion.entries();
-			while (entries.hasMoreElements()) {
-				JarEntry entry = entries.nextElement();
-				JarEntry oldEntry = oldVersion.getJarEntry(entry.getName());
-				if (oldEntry == null || entry.getLastModifiedTime().compareTo(oldEntry.getLastModifiedTime()) > 0) {
-					// TO DO: Copy new entry to an update jar file.
-					System.out.println("Adding " + entry.getName() + " to update " + ".");
+			try (JarOutputStream jos = new JarOutputStream(new FileOutputStream(updateFile))) {
+				Enumeration<JarEntry> entries = newVersion.entries();
+				while (entries.hasMoreElements()) {
+					JarEntry entry = entries.nextElement();
+					JarEntry oldEntry = oldVersion.getJarEntry(entry.getName());
+					if (oldEntry == null || entry.getLastModifiedTime().compareTo(oldEntry.getLastModifiedTime()) > 0) {
+						// TO DO: sign updates.
+						jos.putNextEntry(entry);
+						try (InputStream in = newVersion.getInputStream(entry)) {
+							while ((lenRead = in.read(buffer, 0, buffer.length)) != -1) {
+								jos.write(buffer, 0, lenRead);
+							}
+							jos.closeEntry();
+						}
+						System.out.println("Adding " + entry.getName() + " to update " + ".");
+					}
 				}
 			}
 
@@ -63,8 +87,20 @@ public class Updater {
 			if (!oldShell.equals(newShell)) {
 				Files.move(newShell.toPath(), oldShell.toPath(), StandardCopyOption.REPLACE_EXISTING);
 			}
+
+			System.out.println("Signing update file...");
+			URL keyStoreUrl = ClassLoader.getSystemResource(".keystore");
+			KeyStore keyStore = KeyStore.getInstance("jks");
+
+			System.out.print("Enter password: ");
+			// TO DO: query passwords before doing jar signing.
+			keyStoreUrl.openStream();
+			sun.security.tools.jarsigner.Main.main(new String[] { "-keystore", keyStoreUrl.toExternalForm(), updateFile.toString(), "updater" });
 		} catch (IOException e) {
 			System.out.println("Not a valid jar file.");
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
